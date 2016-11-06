fb01acd9c2a036fea29cde08502a6d76a50ab100
Neil Marklund <nmarklund10@tamu.edu>
2016-11-06 15:39:19 -0600

    finsihed fleet select

--- clientCode/Classes.js
+++ clientCode/Classes.js
@@ -3,13 +3,16 @@ class Player {
         this.homeGrid = new Grid();
 		this.id = -1;
         this.fleet = new Array(4);
+		this.fleet[0] = "Scrambler";
+		this.fleet[1] = "Submarine";
+		this.fleet[2] = "Cruiser";
+		this.fleet[3] = "Executioner";
     }
 }
 
 class buildAFleetWindow {
-	constructor(canvas, FPS, scale) {
+	constructor(canvas, scale) {
 		this.scale = scale;
-		this.FPS = 1000/FPS;
 		this.canvas = canvas;
 		this.context = canvas.getContext('2d');
 		this.background = new Image();
@@ -23,22 +26,67 @@ class buildAFleetWindow {
 		this.class5 = new Image();
 		this.class5.src = 'images/Ships/ship5temp.png';
 		this.buildButtons = document.getElementsByClassName('buildButton');
-		console.log(this.buildButtons);
 	}
 	adjust(dimension) {
 		return dimension * this.scale;
 	}
+	drawButtons() {
+		var window = this;
+		[].forEach.call(this.buildButtons, function(element) {
+			console.log(element.offsetLeft);
+			element.style.left = window.adjust(element.offsetLeft)+'px';
+			element.style.top = window.adjust(element.offsetTop)+'px';
+		});
+	}
 	draw() {
 		this.context.drawImage(this.background, 0, 0, this.adjust(this.background.width), this.adjust(this.background.height));
-		this.context.drawImage(this.class2, this.adjust(50), this.adjust(50), this.adjust(this.class2.width), this.adjust(this.class2.height));
-		this.context.drawImage(this.class3, this.adjust(80), this.adjust(80), this.adjust(this.class3.width), this.adjust(this.class3.height));
-		this.context.drawImage(this.class4, this.adjust(110), this.adjust(110), this.adjust(this.class4.width), this.adjust(this.class4.height));
-		this.context.drawImage(this.class5, this.adjust(140), this.adjust(140), this.adjust(this.class5.width), this.adjust(this.class5.height));
-		[].forEach.call(this.buildButtons, function(element) {
-			console.log(element.style.left);
-//			element.style.left = this.adjust(element.style.left);
-//			element.style.top = this.adjust(element.style.top);
-		})
+		this.context.drawImage(this.class2, this.adjust(180), this.adjust(240), this.adjust(this.class2.width), this.adjust(this.class2.height));
+		this.context.drawImage(this.class3, this.adjust(250), this.adjust(240), this.adjust(this.class3.width), this.adjust(this.class3.height));
+		this.context.drawImage(this.class4, this.adjust(320), this.adjust(240), this.adjust(this.class4.width), this.adjust(this.class4.height));
+		this.context.drawImage(this.class5, this.adjust(390), this.adjust(240), this.adjust(this.class5.width), this.adjust(this.class5.height));
+		this.context.font = 'bold 32px Arial';
+		this.context.fillStyle = 'white';
+		this.context.shadowColor = 'black';
+		this.context.shadowOffsetX = 3;
+		this.context.shadowOffsetY = 3;
+		this.context.fillText('Build Fleet Menu', this.adjust(850), this.adjust(100));
+		this.context.font = '22px Arial';
+		this.context.fillText('Scrambler', this.adjust(850), this.adjust(225));
+		this.context.fillText('Scanner', this.adjust(850), this.adjust(315));
+		this.context.fillText('Submarine', this.adjust(850), this.adjust(435));
+		this.context.fillText('Destroyer', this.adjust(850), this.adjust(525));
+		this.context.fillText('Cruiser', this.adjust(850), this.adjust(645));
+		this.context.fillText('Carrier', this.adjust(850), this.adjust(735));
+		this.context.fillText('Executioner', this.adjust(850), this.adjust(855));
+		this.context.fillText('Artillery', this.adjust(850), this.adjust(945));
+		this.context.font = '28px Arial';
+		this.context.fillText('Class 2', this.adjust(750), this.adjust(170));
+		this.context.fillText('Class 3', this.adjust(750), this.adjust(380));
+		this.context.fillText('Class 4', this.adjust(750), this.adjust(590));
+		this.context.fillText('Class 5', this.adjust(750), this.adjust(800));
+		this.context.font = 'bold 28px Arial';
+		this.context.fillText('Ship Ability', this.adjust(250), this.adjust(750));
+	}
+}
+
+class fleetPositionWindow {
+	constructor(canvas, scale) {
+		this.scale = scale;
+		this.canvas = canvas;
+		this.context = canvas.getContext('2d');
+		this.background = new Image();
+		this.background.src = 'images/Ships/shipSelect.png';
+		this.class2 = new Image();
+		this.class2.src = 'images/Ships/ship2' + client.fleet[0] + '.png';
+		this.class3 = new Image();
+		this.class3.src = 'images/Ships/ship3' + client.fleet[1] + '.png';
+		this.class4 = new Image();
+		this.class4.src = 'images/Ships/ship4' + client.fleet[2] + '.png';
+		this.class5 = new Image();
+		this.class5.src = 'images/Ships/ship5' + client.fleet[3] + '.png';
+	}
+	adjust(dimension) {
+		return dimension * this.scale;
 	}
 }
 
--- clientCode/Main.js
+++ clientCode/Main.js
@@ -2,6 +2,7 @@ var client;
 var enemy;
 var gameID;
 var prepWindow;
+var positionWindow;
 var socket = io.connect();
 var currentGame;
 
@@ -15,9 +16,99 @@ function initialize() {
     client = new Player();
 	enemy = new Player();
 	gameID = -1;
-	prepWindow = new buildAFleetWindow(document.getElementById('buildCanvas'), 30, .75);
+	prepWindow = new buildAFleetWindow(document.getElementById('buildCanvas'), .75);
 }
 
 function loadGame() {
 	prepWindow.draw();
+	prepWindow.drawButtons();
 }
+
+function shipDetails(shipname) {
+	var index = -1;
+	var descriptions = document.getElementsByClassName('shipDes');
+	
+	switch(shipname){
+		case "Scrambler":
+			index = 0;
+			if (prepWindow.class2.src != 'images/Ships/ship2Scrambler.png') {
+				prepWindow.class2.src = 'images/Ships/ship2Scrambler.png';
+				prepWindow.class2.addEventListener('load', prepWindow.draw.bind(prepWindow));
+				client.fleet[0] = "Scrambler";
+			}
+			break;
+		case "Scanner":
+			index = 1;
+			if (prepWindow.class2.src != 'images/Ships/ship2Scanner.png') {
+				prepWindow.class2.src = 'images/Ships/ship2Scanner.png';
+				prepWindow.class2.addEventListener('load', prepWindow.draw.bind(prepWindow));
+				client.fleet[0] = "Scanner";
+			}
+			break;	
+		case "Submarine":
+			index = 2;
+			if (prepWindow.class3.src != 'images/Ships/ship3Submarine.png') {
+				prepWindow.class3.src = 'images/Ships/ship3Submarine.png';
+				prepWindow.class3.addEventListener('load', prepWindow.draw.bind(prepWindow));
+				client.fleet[1] = "Submarine";
+			}
+			break;
+		case "Destroyer":
+			index = 3;
+			if (prepWindow.class3.src != 'images/Ships/ship3Destroyer.png') {
+				prepWindow.class3.src = 'images/Ships/ship3Destroyer.png';
+				prepWindow.class3.addEventListener('load', prepWindow.draw.bind(prepWindow));
+				client.fleet[1] = "Destroyer";
+			}
+			break;
+		case "Cruiser":
+			index = 4;
+			if (prepWindow.class4.src != 'images/Ships/ship4Cruiser.png') {
+				prepWindow.class4.src = 'images/Ships/ship4Cruiser.png';
+				prepWindow.class4.addEventListener('load', prepWindow.draw.bind(prepWindow));
+				client.fleet[2] = "Cruiser";
+			}
+			break;
+		case "Carrier":
+			index = 5;
+			if (prepWindow.class4.src != 'images/Ships/ship4Carrier.png') {
+				prepWindow.class4.src = 'images/Ships/ship4Carrier.png';
+				prepWindow.class4.addEventListener('load', prepWindow.draw.bind(prepWindow));
+				client.fleet[2] = "Carrier";
+			}
+			break;
+		case "Executioner":
+			index = 6;
+			if (prepWindow.class5.src != 'images/Ships/ship5Executioner.png') {
+				prepWindow.class5.src = 'images/Ships/ship5Executioner.png';
+				prepWindow.class5.addEventListener('load', prepWindow.draw.bind(prepWindow));
+				client.fleet[3] = "Executioner";
+			}
+			break;
+		case "Artillery":
+			index = 7;
+			if (prepWindow.class5.src != 'images/Ships/ship5Artillery.png') {
+				prepWindow.class5.src = 'images/Ships/ship5Artillery.png';
+				prepWindow.class5.addEventListener('load', prepWindow.draw.bind(prepWindow));
+				client.fleet[3] = "Artillery";
+			}
+			break;
+		default: 
+			console.log("Ship Detail Button Error");
+	}
+	[].forEach.call(descriptions, function(element){
+			element.style.display = 'none';
+	});
+	descriptions[index].style.display = 'block';
+}
+
+function loadPositionSelect() {
+	
+}
+
+function toPositionSelect() {
+	document.getElementById('buildAFleet').style.display = 'none';
+	document.getElementById('positionFleet').style.display = 'block';
+	loadPositionSelect();
+}
+
