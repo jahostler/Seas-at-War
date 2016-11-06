404084b5e80746b870f4eae2874a1a2a81f3156a
Jacob Hostler <jacob.hostler@tamu.edu>
2016-11-05 20:04:42 -0500

    added instructions page

--- clientCode/Main.js
+++ clientCode/Main.js
@@ -128,6 +128,66 @@ function instructions() {
 	document.getElementById('instructions').style.display = 'block';
 }
 
+function nextInstruction(){
+	if(document.getElementById('i1').style.display == 'block') { //1 -> 2
+		document.getElementById('i1').style.display = 'none';
+		document.getElementById('i2').style.display = 'block';
+	}
+	else if(document.getElementById('i2').style.display == 'block') { //2 -> 3
+		document.getElementById('i2').style.display = 'none';
+		document.getElementById('i3').style.display = 'block';
+	}
+	else if(document.getElementById('i3').style.display == 'block') {
+		document.getElementById('i3').style.display = 'none';
+		document.getElementById('i4').style.display = 'block';
+	}
+	else if(document.getElementById('i4').style.display == 'block') {
+		document.getElementById('i4').style.display = 'none';
+		document.getElementById('i5').style.display = 'block';
+	}
+	else if(document.getElementById('i5').style.display == 'block') {
+		document.getElementById('i5').style.display = 'none';
+		document.getElementById('i6').style.display = 'block';
+	}
+	else if(document.getElementById('i6').style.display == 'block') {
+		document.getElementById('i6').style.display = 'none';
+		document.getElementById('i7').style.display = 'block';
+	}
+	else if(document.getElementById('i7').style.display == 'block') {
+		//do nothing
+	}
+}
+
+function prevInstruction() {
+	if(document.getElementById('i7').style.display == 'block') { //1 -> 2
+		document.getElementById('i7').style.display = 'none';
+		document.getElementById('i6').style.display = 'block';
+	}
+	else if(document.getElementById('i6').style.display == 'block') { //2 -> 3
+		document.getElementById('i6').style.display = 'none';
+		document.getElementById('i5').style.display = 'block';
+	}
+	else if(document.getElementById('i5').style.display == 'block') {
+		document.getElementById('i5').style.display = 'none';
+		document.getElementById('i4').style.display = 'block';
+	}
+	else if(document.getElementById('i4').style.display == 'block') {
+		document.getElementById('i4').style.display = 'none';
+		document.getElementById('i3').style.display = 'block';
+	}
+	else if(document.getElementById('i3').style.display == 'block') {
+		document.getElementById('i3').style.display = 'none';
+		document.getElementById('i2').style.display = 'block';
+	}
+	else if(document.getElementById('i2').style.display == 'block') {
+		document.getElementById('i2').style.display = 'none';
+		document.getElementById('i1').style.display = 'block';
+	}
+	else if(document.getElementById('i1').style.display == 'block') {
+		//do nothing
+	}
+}
+
 function credits() {
 	document.getElementById('mainMenu').style.display = 'none';
 	document.getElementById('credits').style.display = 'block';
